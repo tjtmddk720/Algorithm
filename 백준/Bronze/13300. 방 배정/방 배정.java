c409782f88649559e4969a674c0f9b450514033c
Seo SeungA <83910139+tjtmddk720@users.noreply.github.com>
2025-01-17 01:47:46 +0900

    [Bronze II] Title: 방 배정, Time: 116 ms, Memory: 14580 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // 첫째줄 수학여행에 참가하는 학생 수 n, 한 방에 배정할 수 있는 최대 인원 수 k - 공백 분리
         // n개의 줄에는 학생 성별 s(0-여, 1-남), 학년 y(1~6) - 공백 분리
-        
+
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
@@ -29,7 +29,10 @@ public class Main {
         int cnt = 0;
         for(int i=0; i<2; i++){
             for(int j=1; j<7; j++){
-                cnt += (arr[i][j]+1)/2;
+                cnt += arr[i][j]/k;
+                if(arr[i][j] % k != 0){
+                    cnt++;
+                }
             }
         }
         bw.write(Integer.toString(cnt));
