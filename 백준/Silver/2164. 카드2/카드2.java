07887fde29394849fdf3e02a2cb22574f30e99e8
Seo SeungA <83910139+tjtmddk720@users.noreply.github.com>
2025-02-15 17:07:48 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 45668 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,35 +1,37 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        int result;
-
-        // 카드가 한 장 남을 때까지 반복
-        // 제일 위에 있는 카드를 버림 -> 제일 위에 있는 카드를 제일 아래 카드 밑으로 옮김
-        // 제일 마지막에 남게 되는 카드 출력
-        // N이 1일 때는 1을 출력 1이 아닐 떈 위 규칙을 따라야 함
-        // 카드가 1장보다 많을 떄만 반복해야함 -> while문
-        // 처음 숫자들을 다 큐에 넣음
-        // 버리는 것을 poll로, 밑으로 옮기는 건 poll하고 다시 add하는 걸로 표현
-
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        int num = Integer.parseInt(br.readLine());
         Queue<Integer> queue = new LinkedList<>();
-        for (int i = 1; i <= N; i++) {
+
+        // add offer 차이 - 꽉 찼을 때 예외발생 여부 o x(false 반환)
+        // poll peek remove 차이 - 제거하고 반환, 비어있을 때 null 반환 | 반환 o 제거 x null 반환 | 반환 o 제거 x 예외발생 o
+
+        // 위에서부터 1 ~ num 까지의 숫자
+        // 제일 위에 있는 카드는 버림, 그 다음 카드는 제일 아래로 보냄
+        // 마지막에 남게 되는 수를 구함
+
+        // 버리는 건 poll, 아래로 보내는 건 poll하면서 offer
+
+        for(int i = 1; i <= num; i++){
             queue.add(i);
         }
-        while (queue.size() > 1) {
+
+        while (queue.size() > 1){
             queue.poll();
-            queue.add(queue.poll());
+            queue.offer(queue.poll());
         }
-        result = queue.peek();
-        
-        // 출력
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(Integer.toString(result)); // 정수를 문자열로 변환하여 출력
-        bw.newLine(); // 새로운 줄로 이동
+
+        bw.write(queue.peek()+"");
         bw.close();
+        br.close();
     }
 }
