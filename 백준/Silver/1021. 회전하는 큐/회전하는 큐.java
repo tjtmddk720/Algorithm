ac7dd78b192addde6ea7c26071cc1efb633b4261
Seo SeungA <83910139+tjtmddk720@users.noreply.github.com>
2025-02-23 20:33:12 +0900

    [Silver III] Title: 회전하는 큐, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -23,11 +23,13 @@ public class Main {
         int i = 0;
         int cnt = 0;
         while(inputNum-- > 0){
+            // 뽑아내려는 수의 위치
             int input = Integer.parseInt(inputs[i++]);
 
-            // input 값의 위치
+            // input의 덱에서의 위치
             double position = (double) deque.indexOf(input) / deque.size();
 
+            // 덱의 앞쪽에 가까울 때 이동
             if(position < 0.5){
                 while(deque.peek() != input){
                 // 왼쪽으로 이동
@@ -35,6 +37,7 @@ public class Main {
                 cnt++;
                 }
             }
+            // 덱의 뒤쪽에 가까울 때 이동
             else {
                 while(deque.peek() != input){
                     // 오른쪽으로 이동
@@ -42,6 +45,7 @@ public class Main {
                     cnt++;
                 }
             }
+            // 이동 후 덱에서 뽑음
             deque.poll();
         }
         bw.write(cnt+"");
